62c8e21f27a6d92a72c93445e586f233b755827b
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-01-16 14:52:41 -0300

    Projeto Inicio

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, StyleSheet, TextInput, Button, Image, TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+    
+    }
+    
+  }
+
+  render(){  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    paddingTop:20,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'blue'
   },
-});
+  
+})
+
+export default App;
+
