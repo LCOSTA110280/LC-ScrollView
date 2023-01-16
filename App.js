d4fbf965e3c93678c69c784c6315355daaa0f63e
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-01-16 15:25:51 -0300

    Exemplo Full

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { Text, View, StyleSheet, TextInput, Button, Image, TouchableOpacity, Alert } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Button, Image, TouchableOpacity, Alert, ScrollView } from 'react-native';
 
 class App extends Component {
   constructor(props){
@@ -14,7 +14,12 @@ class App extends Component {
   render(){  
   return (
     <View style={styles.container}>
-   
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+      </ScrollView>
     </View>
   );
 }
@@ -24,10 +29,28 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     paddingTop:20,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor:'blue'
   },
+  box1:{
+    backgroundColor:'red',
+    height:250,
+    width:150,
+  },
+  box2:{
+    backgroundColor:'yellow',
+    height:250,
+    width:150,
+  },
+  box3:{
+    backgroundColor:'green',
+    height:250,
+    width:150,
+  },
+  box4:{
+    backgroundColor:'blue',
+    height:250,
+    width:150,
+  },
+  
   
 })
 
